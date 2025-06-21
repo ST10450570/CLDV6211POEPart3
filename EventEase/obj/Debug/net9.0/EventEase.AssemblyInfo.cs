f368e5834f9e569ec42611d739f988b92c19eb1f
ST10450570 <St10450570@imconnect.edu.za>
2025-06-21 19:50:52 +0200

    commit

--- EventEase/obj/Debug/net9.0/EventEase.AssemblyInfo.cs
+++ EventEase/obj/Debug/net9.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a6d5de621853b3efadb768e0b8452b452c5b13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ed36414340e0b0ce281ba794d4d7ea180c4a93")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
